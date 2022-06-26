48b56bc6b1a7e9541508edc873626d5f4c94252c
Jeremiah Quill <jcq5010@gmail.com>
2022-06-26 17:18:46 -0400

    added types for RecipeList

--- client/components/RecipeList.tsx
+++ client/components/RecipeList.tsx
@@ -1,6 +1,15 @@
 import RecipeCard from "./RecipeCard";
 
-const RecipeList = ({ recipes }: { recipes: any }) => {
+type Recipe = {
+  id: string;
+  name: string;
+  ingredients: string;
+  instructions: string;
+  publishedAt: number;
+  photo: string | null;
+};
+
+const RecipeList = ({ recipes }: { recipes: Recipe[] }) => {
   return (
     <ul className="flex flex-col gap-2">
       {recipes.map((recipe: any) => (
