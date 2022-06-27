1ffea2d50d9b5db6686a1e63b5aba1224fddbaa4
Jeremiah Quill <jcq5010@gmail.com>
2022-06-27 10:05:15 -0400

    fixed GridItem typing

--- client/components/GridItem.tsx
+++ client/components/GridItem.tsx
@@ -3,11 +3,15 @@ import React from "react";
 type GridItemProps = {
   colSpan?: string;
   rowSpan?: string;
-  children: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 // TODO: refactor types
-const GridItem = ({ colSpan = "col-span-1", rowSpan = "row-span-1", children }: GridItemProps) => {
+const GridItem = ({
+  colSpan = "col-span-1",
+  rowSpan = "row-span-1",
+  children = null,
+}: GridItemProps) => {
   return (
     <div
       className={`${colSpan} ${rowSpan} border-2 rounded-lg border-orange-400 flex justify-center items-center h-full w-full hover:scale-105 hover:shadow-lg hover:shadow-black transition-all`}>
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -11,18 +11,18 @@ const Home: NextPage = () => {
     <div className="flex items-center justify-center h-full w-full bg-gray-600">
       <div className="md:aspect-[1.5/1] grid gap-2 grid-cols-3 w-full h-full md:grid-cols-6 md:grid-rows-5 md:h-auto">
         {/* Spotify */}
-        <GridItem colSpan={1} rowSpan={1}>
+        <GridItem>
           <IconLink
             src={"/spotify.svg"}
             url="https://open.spotify.com/user/jcq5010?si=531f0314cf8f462c"
           />
         </GridItem>
         {/* Email */}
-        <GridItem colSpan={1} rowSpan={1}>
+        <GridItem>
           <IconLink src={"/email.svg"} url="https://www.email.com" />
         </GridItem>
         {/* Github */}
-        <GridItem colSpan={1} rowSpan={1}>
+        <GridItem>
           <IconLink src={"/github.svg"} url="https://github.com/jeremiah-quill" />
         </GridItem>
         <GridItem colSpan={"col-span-3"} rowSpan={"row-span-2"}>
@@ -33,29 +33,29 @@ const Home: NextPage = () => {
           <Location />
         </GridItem>
         {/* LinkedIn */}
-        <GridItem colSpan={1} rowSpan={1}>
+        <GridItem>
           <IconLink
             src={"/linkedin.svg"}
             url="https://www.linkedin.com/in/jeremiah-quill-281b06122/"
           />
         </GridItem>
         {/* Recipes */}
-        <GridItem colSpan={1} rowSpan={1}>
+        <GridItem>
           <IconInternalLink src={"/cookbookios.png"} url="/recipes" />
         </GridItem>
-        <GridItem colSpan={1} rowSpan={1} children={null} />
-        <GridItem colSpan={1} rowSpan={1} children={null} />
-        <GridItem colSpan={1} rowSpan={1} children={null} />
-        <GridItem colSpan={1} rowSpan={1} children={null} />
-        <GridItem colSpan={1} rowSpan={"row-span-2"} children={null} />
-        <GridItem colSpan={1} rowSpan={1} children={null} />
-        <GridItem colSpan={1} rowSpan={1} children={null} />
-        <GridItem colSpan={1} rowSpan={1} children={null} />
-        <GridItem colSpan={1} rowSpan={1} children={null} />
-        <GridItem colSpan={1} rowSpan={1} children={null} />
-        <GridItem colSpan={"col-span-2"} rowSpan={1} children={null} />
-        <GridItem colSpan={1} rowSpan={1} children={null} />
-        <GridItem colSpan={1} rowSpan={1} children={null} />
+        <GridItem />
+        <GridItem />
+        <GridItem />
+        <GridItem />
+        <GridItem rowSpan={"row-span-2"} />
+        <GridItem />
+        <GridItem />
+        <GridItem />
+        <GridItem />
+        <GridItem />
+        <GridItem colSpan={"col-span-2"} />
+        <GridItem />
+        <GridItem />
       </div>
     </div>
   );
