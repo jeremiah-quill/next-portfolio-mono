f8ecb9ecb11935c7224e852015e763fb63ac32c4
Jeremiah Quill <jcq5010@gmail.com>
2022-06-30 11:38:53 -0400

    added rel noreferrer to iconlink component

--- client/components/IconLink.tsx
+++ client/components/IconLink.tsx
@@ -1,6 +1,6 @@
 const IconLink = ({ src, url, title }: { src: string; url: string; title?: string }) => {
   return (
-    <a className="w-full h-full" href={url} target="_blank">
+    <a className="w-full h-full" href={url} target="_blank" rel="noreferrer">
       <div className="text-center flex flex-col h-full items-center justify-center gap-1">
         <img src={src} className="w-1/2" />
         <h1 className="font-thin font-robot text-xl">{title}</h1>
